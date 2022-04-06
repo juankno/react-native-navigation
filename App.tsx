01f93be4508fcafa827ef24aa805114eca799ba1
Juan Esteban <juanes0202@gmail.com>
2022-04-06 14:32:44 -0500

    inicializando stack Navigator

--- App.tsx
+++ App.tsx
@@ -1,28 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text style={styles.title}>App Screen</Text>
-      </View>
+        <StackNavigator/>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    alignSelf: 'center',
-  },
-});
