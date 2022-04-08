4bd836ea84213e4d35981fe232127486c3c8d696
Juan Esteban <juanes0202@gmail.com>
2022-04-08 09:41:00 -0500

    instalando y configurando el drawer navigator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './src/navigator/StackNavigator';
+import { DrawerNavigator } from './src/navigator/DrawerNavigator';
+// import { StackNavigator } from './src/navigator/StackNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-        <StackNavigator/>
+        {/* <StackNavigator/> */}
+        <DrawerNavigator/>
     </NavigationContainer>
   );
 };
