9562c7757d139c332a405b40d5d33b53055d30f7
Juan Esteban <juanes0202@gmail.com>
2022-04-08 14:13:33 -0500

    ocultando la barra de navegación del drawer que viene por defecto

--- src/navigator/DrawerNavigator.tsx
+++ src/navigator/DrawerNavigator.tsx
@@ -2,14 +2,23 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { SettingsScreen } from '../screens/SettingsScreen';
+import { Dimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
+const { width } = Dimensions.get('window');
+
+
 export const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    <Drawer.Navigator
+      screenOptions={{
+        drawerType: width >= 768 ? 'permanent' : 'front',
+        headerShown: false,
+      }}
+    >
+      <Drawer.Screen name="StackNavigator" options={{ title: 'Home' }} component={StackNavigator} />
+      <Drawer.Screen name="SettingsScreen" options={{ title: 'Settings' }} component={SettingsScreen} />
     </Drawer.Navigator>
   );
 };
