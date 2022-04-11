79a6299c68080590b3606bafeb25e3e33e48cf69
Juan Esteban <juanes0202@gmail.com>
2022-04-11 11:38:23 -0500

    configurando material tap bottom para android

--- src/navigator/BottomTabsNavigator.tsx
+++ src/navigator/BottomTabsNavigator.tsx
@@ -1,16 +1,89 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { TabOneScreen } from '../screens/TabOneScreen';
 import { TabTwoScreen } from '../screens/TabTwoScreen';
 import { StackNavigator } from './StackNavigator';
 import { colors } from '../theme/appTheme';
-import { Text } from 'react-native';
+import { Text, Platform } from 'react-native';
 
-const Tab = createBottomTabNavigator();
 
 export const BottomTabsNavigator = () => {
+
+    return Platform.OS === 'ios'
+        ? <TabsIos />
+        : <TabsAndroid />;
+
+};
+
+
+const BottomTapAndroid = createMaterialBottomTabNavigator();
+
+const TabsAndroid = () => {
     return (
-        <Tab.Navigator
+        <BottomTapAndroid.Navigator
+            sceneAnimationEnabled={true}
+            barStyle={{
+                backgroundColor: colors.primary,
+            }}
+            screenOptions={({ route }) => ({
+                tabBarActiveTintColor: colors.primary,
+                headerShown: false,
+                tabBarStyle: {
+                    borderTopColor: colors.primary,
+                    borderTopWidth: 0,
+                    elevation: 0,
+                },
+                tabBarLabelStyle: {
+                    fontSize: 15,
+                },
+
+                tabBarIcon: ({ color, focused }) => {
+
+                    let iconName: string = '';
+                    switch (route.name) {
+                        case 'TabOneScreen':
+                            iconName = 'T1';
+                            break;
+                        case 'TabTwoScreen':
+                            iconName = 'T2';
+                            break;
+                        case 'StackNavigator':
+                            iconName = 'St';
+                            break;
+                    }
+                    return <Text style={{ color }}>{iconName}</Text>;
+                },
+            })}
+        >
+            <BottomTapAndroid.Screen
+                name="TabOneScreen" options={{
+                    title: 'Tab One',
+                }}
+                component={TabOneScreen}
+            />
+            <BottomTapAndroid.Screen
+                name="TabTwoScreen" options={{
+                    title: 'Tab Two',
+                }}
+                component={TabTwoScreen}
+            />
+            <BottomTapAndroid.Screen
+                name="StackNavigator" options={{
+                    title: 'Stack',
+                }}
+                component={StackNavigator}
+            />
+        </BottomTapAndroid.Navigator>
+    );
+};
+
+
+const BottomTapIos = createBottomTabNavigator();
+
+const TabsIos = () => {
+    return (
+        <BottomTapIos.Navigator
             sceneContainerStyle={{
                 backgroundColor: 'white',
             }}
@@ -44,30 +117,24 @@ export const BottomTabsNavigator = () => {
                 },
             })}
         >
-            <Tab.Screen
-                name="TabOneScreen"
-                options={{
+            <BottomTapIos.Screen
+                name="TabOneScreen" options={{
                     title: 'Tab One',
-                    tabBarLabel: 'Tab One',
                 }}
                 component={TabOneScreen}
             />
-            <Tab.Screen
-                name="TabTwoScreen"
-                options={{
+            <BottomTapIos.Screen
+                name="TabTwoScreen" options={{
                     title: 'Tab Two',
-                    tabBarLabel: 'Tab Two',
                 }}
                 component={TabTwoScreen}
             />
-            <Tab.Screen
-                name="StackNavigator"
-                options={{
+            <BottomTapIos.Screen
+                name="StackNavigator" options={{
                     title: 'Stack',
-                    tabBarLabel: 'Stack',
                 }}
                 component={StackNavigator}
             />
-        </Tab.Navigator>
+        </BottomTapIos.Navigator>
     );
 };
