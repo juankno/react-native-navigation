9a003f9a6465748ac0173d86dfc1a58b495b42aa
Juan Esteban <juanes0202@gmail.com>
2022-04-19 12:33:57 -0500

    llamando metodo des ibicio de sesión desde el context de auth

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button } from 'react-native';
+import { styles, colors } from '../theme/appTheme';
+import { AuthContext } from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+
+  const { signIn, authState } = useContext(AuthContext);
+
   return (
-    <View>
-        <Text>Contacts Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Contacts Screen</Text>
+
+      {!authState.isLoggedIn &&
+        <Button
+          title="Sign in"
+          onPress={signIn}
+          color={colors.primary}
+        />
+      }
+
     </View>
   );
 };
